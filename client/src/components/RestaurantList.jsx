997d24335cef65b7cdd84281b3eddbb0d80c1b5a
crisluda <crisluda2244@gmail.com>
2020-09-02 15:29:34 +0000

    to show all restaurant

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useContext } from "react";
 import api from "../apis/api";
 import { RestaurantsContext } from "../context/RestaurantsContext";
+import { useHistory } from "react-router-dom";
+import "./css/style.css";
 
 function RestaurantList(props) {
   const { restaurants, setRestaurants } = useContext(RestaurantsContext);
+  let history = useHistory();
   useEffect(() => {
     const apicall = async () => {
       try {
@@ -15,6 +18,24 @@ function RestaurantList(props) {
     };
     apicall();
   }, []);
+
+  async function handleDelete(id) {
+    try {
+      const response = await api.delete(`/${id}`);
+      setRestaurants(
+        restaurants.filter((restaurant) => {
+          return restaurant.id !== id;
+        })
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  function handleUpdate(id) {
+    history.push(`/restaurants/${id}/update`);
+  }
+
   return (
     <div className="list-group">
       <table className="table table-hover table-dark">
@@ -38,10 +59,20 @@ function RestaurantList(props) {
                   <td>{"$".repeat(restaurant.price_range)}</td>
                   <td>reviews</td>
                   <td>
-                    <button className="btn btn-warning">Update</button>
+                    <button
+                      onClick={() => handleUpdate(restaurant.id)}
+                      className="btn btn-warning"
+                    >
+                      Update
+                    </button>
                   </td>
                   <td>
-                    <button className="btn btn-danger">Delete</button>
+                    <button
+                      onClick={() => handleDelete(restaurant.id)}
+                      className="btn btn-danger"
+                    >
+                      Delete
+                    </button>
                   </td>
                 </tr>
               );
