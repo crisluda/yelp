5018196b4422b959c68d707da3f0d45effc2aff4
crisluda <crisluda2244@gmail.com>
2020-09-12 20:15:46 +0000

    add the selectedRestaurant context

--- client/src/context/RestaurantsContext.js
+++ client/src/context/RestaurantsContext.js
@@ -3,14 +3,21 @@ import React, { useState, createContext } from "react";
 export const RestaurantsContext = createContext();
 export const RestaurantContextProvider = (props) => {
   const [restaurants, setRestaurants] = useState([]);
+  const [selectedRestaurant, setSelectedRestaurant] = useState(null);
   const addRestaurants = (restaurant) => {
     setRestaurants([...restaurants, restaurant]);
   };
   return (
     <RestaurantsContext.Provider
-      value={{ restaurants: restaurants, setRestaurants, addRestaurants }}
+      value={{
+        restaurants: restaurants,
+        setRestaurants,
+        addRestaurants,
+        selectedRestaurant,
+        setSelectedRestaurant,
+      }}
     >
-      {props.children}
+      {props.children}{" "}
     </RestaurantsContext.Provider>
   );
 };
