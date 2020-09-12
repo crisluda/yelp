c799f1c314728d769351cd787522fd49b306d7e4
crisluda <crisluda2244@gmail.com>
2020-09-12 20:17:03 +0000

    add the restaurant name to the detail page

--- client/src/routes/RestaurantsDetailPage.jsx
+++ client/src/routes/RestaurantsDetailPage.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import api from "../apis/api";
+import { useParams } from "react-router-dom";
+import { RestaurantsContext } from "../context/RestaurantsContext";
 
 function RestaurantsDetailPage() {
-  return <div>Detail Page</div>;
+  const { id } = useParams();
+  const { selectedRestaurant, setSelectedRestaurant } = useContext(
+    RestaurantsContext
+  );
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await api.get(`/${id}`);
+        setSelectedRestaurant(response.data.data.restaurants);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  // let history = useHistory();
+  return (
+    <div>
+      <h1>{selectedRestaurant && selectedRestaurant.name}</h1>
+    </div>
+  );
 }
 
 export default RestaurantsDetailPage;
